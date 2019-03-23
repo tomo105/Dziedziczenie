425b4026eb5f4c4349ac95ce798781d03f2d23c7
tomo105 <tomo105@wp.pl>
2019-03-23 22:29:25 +0100

    add singleton-project pattern2

--- src/Singleton/Main.java
+++ src/Singleton/Main.java
@@ -4,7 +4,9 @@ public class Main {
     public static void main(String[] args) {
         Lunch lunch = new Lunch();
         lunch.TestPrivate();
+
         lunch.TestSingleton();
+        lunch.TestStatic();
         lunch.TestStatic();
 
     }
--- src/Singleton/Soup1.java
+++ src/Singleton/Soup1.java
@@ -6,6 +6,7 @@ public class Soup1 {
     }
     //udostepnienie konstrukcji przy pomocy metody statycznje
     public static Soup1 makeSoup() {
+        System.out.println("create object by static function");
         return new Soup1();
     }
 }
